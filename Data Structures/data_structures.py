90052b5cb084aabe44a4f227d184f42d86e2bc30
Eyal Schuller <eschullert@gmail.com>
2019-03-22 14:49:26 -0600

    first commit

---
+++ Data Structures/data_structures.py
@@ -0,0 +1,26 @@
+'''
+Implementación de varias estructuras de datos en Python
+'''
+class Stack:
+    def _init_(self):
+        return 'a'
+
+class Queue:
+    def _init_(self):
+        return 'a'
+
+class Set:
+    def _init_(self):
+        return 'a'
+
+class LinkedList:
+    def _init_(self):
+        return 'a'
+
+class BinaryTree:
+    def _init_(self):
+        return 'a'
+
+class AVLTree:
+    def _init_(self):
+        return 'a'
---
+++ PUBG/readme.md
@@ -0,0 +1,1 @@
+Es importante bajar antes los datasets de https://www.kaggle.com/c/pubg-finish-placement-prediction/data
